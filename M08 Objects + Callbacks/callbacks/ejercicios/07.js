82d99de3950df0eb2ac733a8fdd470e5a85e8935
Fernando_Velazco <daniel.fernando.velazco@gmail.com>
2024-01-26 18:17:27 -0500

    Se resuelve el ejercicio 07

--- M08 Objects + Callbacks/callbacks/ejercicios/07.js
+++ M08 Objects + Callbacks/callbacks/ejercicios/07.js
@@ -2,6 +2,16 @@ function filter(arrayOfStrings) {
   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
   // Luego retorna un nuevo arreglo con estos elementos.
   // Tu código:
+
+  let arreglo = [];
+
+  for (let elemento of arrayOfStrings) {
+
+    elemento = elemento.toLowerCase().split('');
+    if(elemento[0]==='a') arreglo.push(elemento.join(''));
+  };
+
+  return arreglo;
 }
 
 module.exports = filter;
