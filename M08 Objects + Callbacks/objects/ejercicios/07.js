fef4637e5d958cced4ef8496ffd22ce00e42ddaa
Fernando_Velazco <daniel.fernando.velazco@gmail.com>
2024-01-27 18:06:39 -0500

    Se resuelve el ejemplo 07

--- M08 Objects + Callbacks/objects/ejercicios/07.js
+++ M08 Objects + Callbacks/objects/ejercicios/07.js
@@ -1,5 +1,17 @@
 // Crea un objeto con propiedades anidadas y un método que acceda a una de las propiedades anidadas utilizando `this`.
 
-const objetoAnidado = {};
+const objetoAnidado = {
+    propiedad1: ' ',
+    propiedad2: 25,
+    propiedad3: {
+        propiedad4: ' ',
+        propiedad5: 74,
+        propiedad6: {
+            propiedad9: function propiedad9() {
+                return 'valor7'
+            }
+        }
+    }    
+};
 
 module.exports = objetoAnidado;
